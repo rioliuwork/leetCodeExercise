bed4b9ef3b34f53ba96109930179708911d95b2f
刘爽 <1358560919@qq.com>
2021-04-19 11:04:40 +0800

    leetCode第1822题练习

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import "leetCodeExercise/exercise"
 
 func main() {
-	var nums = []int{3, 2, 3, 3, 3}
-	len := exercise.RemoveElement(nums, 3)
-	for i := 0; i < len; i++ {
-		print(nums[i])
-	}
+	var nums = []int{9, 72, 34, 29, -49, -22, -77, -17, -66, -75, -44, -30, 0}
+	val := exercise.ArraySign(nums)
+	print(val)
 }
