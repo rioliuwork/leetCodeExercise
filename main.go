a3155ec4629dde9c19eef524372f1abf2bb0497b
刘爽 <1358560919@qq.com>
2021-04-30 15:58:27 +0800

    leetCode704. 二分查找

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "leetCodeExercise/exercise"
 
 func main() {
-	haystack := 234
-	val := exercise.SubtractProductAndSum(haystack)
+	nums := []int{-1, 0, 2, 3, 5, 9, 12}
+	val := exercise.Search(nums, 12)
 	print(val)
 }
