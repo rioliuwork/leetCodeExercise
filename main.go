9ca3318a5f2db230288a2a10309831385e57e1fd
刘爽 <1358560919@qq.com>
2021-05-06 11:53:24 +0800

    leetCode1844. 将所有数字用字符替换

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "leetCodeExercise/exercise"
 
 func main() {
-	nums := []int{8, 1, 2, 2, 3}
-	val := exercise.SmallerNumbersThanCurrent(nums)
+	nums := "a1b2c3d4e"
+	val := exercise.ReplaceDigits(nums)
 	print(val)
 }
