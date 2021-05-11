98750e66dc2254a489a3e010ce141fc2b13e75d1
刘爽 <1358560919@qq.com>
2021-05-11 15:46:13 +0800

    leetCode面试题 17.16. 按摩师

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "leetCodeExercise/exercise"
 
 func main() {
-	points := []int{-2, 0, 3, -5, 2, -1}
-	val := exercise.Constructor(points)
-	v := val.SumRange(0, 2)
-	print(v)
+	points := []int{2, 7, 9, 3, 7, 1}
+	val := exercise.Massage(points)
+	print(val)
 }
